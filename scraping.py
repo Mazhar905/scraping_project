5181b522f042ebe47c3766fcdd37aafd6e58c4b9
Mazhar905 <gujjarmazhar90@gmail.com>
2023-05-15 23:27:30 +0500

    Update the script

--- scraping.py
+++ scraping.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import re
 from selenium import webdriver
@@ -7,7 +8,16 @@ from selenium.webdriver.support import expected_conditions as EC
 from fake_useragent import UserAgent
 from concurrent.futures import ThreadPoolExecutor
 
-def scrape_data(url):
+def scrape_data(token):
+    start_time = time.time()
+    print("INFO ---------- Starting Time:", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time)))
+    # token = input("Enter the token Please.")
+    website_url = 'https://www.dextools.io/app/en/ether/pair-explorer/'
+    if website_url in token:
+        url = token
+    else:
+        url = website_url + token    
+
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument("--incognito")
@@ -34,15 +44,24 @@ def scrape_data(url):
         if match:
             market_cap = match.group(1)
             print('Total Market Cap: $', market_cap)
-            return url, 'Total Market Cap: $' + market_cap
+            # return url, 'Total Market Cap: $' + market_cap
         else:
-            print("Total Market Cap not found")
-            return url, None
+            print("Total Market Cap: $0")
+            # return url, None
+            market_cap = 0
     except Exception as e:
-        print(f"Error occurred for URL {url}:")
+        print(f"Error occurred for URL {url} or Seems the given token is not correct")
         return url, None
     finally:
         driver.quit()
+    
+    end_time = time.time()
+    print("INFO ---------- Ending Time:", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(end_time)))
+    duration = end_time - start_time
+    print("Execution Time (in seconds):", duration)
+    print("Execution Time (in minutes):", duration / 60)
+    return market_cap
+
 
 
 def extractValue(fName):
@@ -74,5 +93,11 @@ def extractValue(fName):
     print("Execution Time (in minutes):", duration / 60)
 
 
-fileName = 'token_list.csv'
-extractValue(fileName)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Parameter is required to run this file")
+    else:
+        parameter = sys.argv[1]
+        # Use the 'parameter' variable to process the URL as required
+        scrape_data(parameter)
